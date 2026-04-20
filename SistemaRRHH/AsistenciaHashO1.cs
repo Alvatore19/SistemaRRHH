d68dc73a30bda28fb59e33aef5505e951208dd62
20150195 <105887488+Happy162006@users.noreply.github.com>
2026-04-20 14:23:19 -0600

    Asistencia Directo 85%
    Solo me falta agregar bien lo del hash

---
+++ SistemaRRHH/AsistenciaHashO1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaRRHH
+{
+    public class AsistenciaHashO1
+    {
+        private Dictionary<string, Asistencia> tabla = new Dictionary<string, Asistencia>();
+
+        private string Key(string idEmpleado, DateTime fecha)
+        {
+            return $"{idEmpleado}_{fecha:yyyyMMdd}";
+        }
+
+        public void Guardar(Asistencia a)
+        {
+            tabla[Key(a.IdEmpleado, a.Fecha)] = a;
+        }
+
+        public Asistencia Obtener(string idEmpleado, DateTime fecha)
+        {
+            string k = Key(idEmpleado, fecha);
+
+            if (tabla.ContainsKey(k))
+                return tabla[k];
+
+            return null;
+        }
+
+        public List<Asistencia> Listar()
+        {
+            return new List<Asistencia>(tabla.Values);
+        }
+    }
+}
